ae6843daf5337c4635a58a9a3c6b1b0dddc87479
pavelbeard <pavelborodin1995@gmail.com>
2024-11-21 22:23:22 +0100

    Eleventh

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,10 +1,25 @@
 import clsx from "clsx";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Layout from "../Layout";
 import ContactMeIcon from "./icons/ContactMeIcon";
 
+const MY_EMAIL = "heavycream9090@icloud.com";
+
 const ContactForm = () => {
   const { t } = useTranslation();
+  const [formData, setFormData] = useState({ subject: "", body: "" });
+  const changeData = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const mailtoLink = `mailto:${MY_EMAIL}?subject=${encodeURIComponent(
+      formData.subject,
+    )}&body=${encodeURIComponent(`${formData.body}\n\n<Your sign>`)}`;
+
+    window.location.href = mailtoLink;
+  };
+
   return (
     <Layout className="flex flex-col text-slate-900 dark:text-slate-300 px-16 mt-20">
       <p
@@ -13,10 +28,7 @@ const ContactForm = () => {
       >
         <ContactMeIcon className="size-8" /> {t("CONTACT_ME.TITLE")}
       </p>
-      <form
-        className="flex flex-col gap-y-4 mt-8"
-        action="mailto:heavycream9090@icloud.com"
-      >
+      <form className="flex flex-col gap-y-4 mt-8" onSubmit={handleSubmit}>
         <label
           className="inline-flex self-start flex-col gap-y-2 text-md md:text-lg xl:text-xl"
           htmlFor="subject"
@@ -27,6 +39,9 @@ const ContactForm = () => {
             id="subject"
             name="subject"
             type="text"
+            value={formData.subject}
+            placeholder={t("CONTACT_ME.SUBJECT_PLACEHOLDER")}
+            onChange={changeData}
           />
         </label>
         <label
@@ -38,6 +53,9 @@ const ContactForm = () => {
             className="p-4 rounded-lg outline-none focus:outline-indigo-400 bg-slate-100/50"
             name="body"
             id="body"
+            value={formData.body}
+            placeholder={t("CONTACT_ME.BODY_PLACEHOLDER")}
+            onChange={changeData}
           ></textarea>
         </label>
         <button
