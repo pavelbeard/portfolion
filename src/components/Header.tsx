860dc6c191222de0b0ca2c2b5024d0e3e026aa26
pavelbeard <pavelborodin1995@gmail.com>
2024-11-24 14:49:01 +0100

    Fourteenth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import clsx from "clsx";
-import { RefObject, useRef } from "react";
+import { RefObject } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import useClickOutside from "../lib/hooks/useClickOutside";
 import useHeader from "../lib/hooks/useHeader";
 import ChangeLangBtn from "./ChangeLangBtn";
 import ColorModeBtn from "./ColorModeBtn";
@@ -9,38 +10,69 @@ import "./HeaderAnimation.css";
 import Burger from "./icons/Burger";
 import XIcon from "./icons/XIcon";
 import "./MenuAnimations.css";
-import useClickOutside from "../lib/hooks/useClickOutside";
 
 const Header = () => {
   const { t } = useTranslation();
-  const { isBurgerOpen, toggleBurger, isDesktop } = useHeader();
+  const { isBurgerOpen, toggleBurger, isDesktop, headerRef } = useHeader();
   const ref = useClickOutside(() => closeBurger());
-  
+
   const closeBurger = () => {
     ref.current?.classList.replace("menu-expand", "menu-collapse");
     setTimeout(() => toggleBurger(), 240);
   };
 
-
   return (
     <header
+      role="navigation"
       className={clsx(
         "fixed top-0 mt-4 w-full flex items-center z-50",
         isDesktop ? "mx-auto justify-center" : "ml-4 justify-start",
       )}
     >
       {isDesktop ? (
-        <nav className="relative nav-header px-8 py-4 flex items-center gap-x-4 text-slate-900 dark:text-slate-300">
-          <Link className="hover:text-slate-500 hover:dark:text-slate-300" to="/#experience">{t("HEADER.EXPERIENCE")}</Link>
-          <Link className="hover:text-slate-500 hover:dark:text-slate-300" to="/#projects">{t("HEADER.PROJECTS")}</Link>
-          <Link className="hover:text-slate-500 hover:dark:text-slate-300" to="/#tech-stack">{t("HEADER.TECH_STACK")}</Link>
-          <Link className="hover:text-slate-500 hover:dark:text-slate-300" to="/#about-me">{t("HEADER.ABOUT_ME")}</Link>
-          <Link className="hover:text-slate-500 hover:dark:text-slate-300" to="/#contacts">{t("HEADER.CONTACTS")}</Link>
+        <nav
+          id="desktop-nav"
+          ref={headerRef}
+          className="relative nav-header px-8 py-4 flex items-center gap-x-4 text-slate-900 dark:text-slate-300"
+        >
+          <Link
+            className="hover:text-slate-500 hover:dark:text-slate-300"
+            to="/#experience"
+          >
+            {t("HEADER.EXPERIENCE")}
+          </Link>
+          <Link
+            className="hover:text-slate-500 hover:dark:text-slate-300"
+            to="/#projects"
+          >
+            {t("HEADER.PROJECTS")}
+          </Link>
+          <Link
+            className="hover:text-slate-500 hover:dark:text-slate-300"
+            to="/#tech-stack"
+          >
+            {t("HEADER.TECH_STACK")}
+          </Link>
+          <Link
+            className="hover:text-slate-500 hover:dark:text-slate-300"
+            to="/#about-me"
+          >
+            {t("HEADER.ABOUT_ME")}
+          </Link>
+          <Link
+            className="hover:text-slate-500 hover:dark:text-slate-300"
+            to="/#contacts"
+          >
+            {t("HEADER.CONTACTS")}
+          </Link>
           <ChangeLangBtn />
           <ColorModeBtn />
         </nav>
       ) : isBurgerOpen ? (
-        <nav className="flex flex-col gap-y-4 text-slate-900 dark:text-slate-300">
+        <nav
+          id="mobile-nav"
+          className="flex flex-col gap-y-4 text-slate-900 dark:text-slate-300"
+        >
           <button onClick={closeBurger}>
             <XIcon />
           </button>
@@ -60,7 +92,12 @@ const Header = () => {
             >
               {t("HEADER.PROJECTS")}
             </Link>
-            <Link className="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-500" to="/#tech-stack">{t("HEADER.TECH_STACK")}</Link>
+            <Link
+              className="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-500"
+              to="/#tech-stack"
+            >
+              {t("HEADER.TECH_STACK")}
+            </Link>
             <Link
               className="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-500"
               to="/#about-me"
--- src/lib/hooks/useHeader.ts
+++ src/lib/hooks/useHeader.ts
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router";
 import useDesktopBreakpoint from "./useDesktopBreakpoint";
 
 export default function useHeader() {
   const [isBurgerOpen, setBurgerOpen] = useState(false);
   const isDesktop = useDesktopBreakpoint();
+  const headerRef = useRef<HTMLElement>(null);
   const { hash } = useLocation();
 
   useEffect(() => {
@@ -15,9 +16,38 @@ export default function useHeader() {
     if (isDesktop) setBurgerOpen(false);
   }, [isDesktop]);
 
+  useEffect(() => {
+    if (headerRef.current) {
+      const $navigation = headerRef.current;
+      const $rootStyle = getComputedStyle(document.documentElement);
+
+      if (!document.body.style.getPropertyValue("animation-timeline")) {
+        $navigation.animate(
+          [
+            { padding: "16px 128px" },
+            {
+              borderRadius: "25px",
+              backdropFilter: "blur(5px)",
+              background: $rootStyle.getPropertyValue("--bg-color"),
+            },
+          ],
+          {
+            // @ts-ignore
+            timeline: new ScrollTimeline({
+              source: document.documentElement,
+            }),
+            // @ts-ignore
+            rangeStart: new CSSUnitValue(50, "px"),
+          },
+        );
+      }
+    }
+  }, [headerRef, isDesktop]);
+
   return {
     isBurgerOpen,
     toggleBurger: () => setBurgerOpen(!isBurgerOpen),
     isDesktop,
+    headerRef,
   };
 }
