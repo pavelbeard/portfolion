7ff6a30a1f07f80ce7599f9ca41b21d81055b693
pavelbeard <pavelborodin1995@gmail.com>
2024-11-24 11:59:20 +0100

    Thirteenth

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -17,7 +17,7 @@ const Project = ({ title, description, link, github, img, tags }: Project) => {
       >
         <img
           className={clsx(
-            "object-cover object-top",
+            "object-cover",
             "h-48 w-64 lg:h-64 lg:w-80 transition duration-300 ease-in",
             "group-hover:scale-[1.02] group-hover:translate-x-[2px] group-hover:-translate-y-[2px]",
             "group-hover:shadow-xl group-hover:shadow-red-600/20 group-hover:dark:shadow-red-800",
@@ -26,7 +26,7 @@ const Project = ({ title, description, link, github, img, tags }: Project) => {
           alt={title}
         />
       </div>
-      <div className="flex flex-col justify-between gap-y-4 items-start">
+      <div className="flex flex-col gap-y-4 items-start">
         <div className="flex flex-col gap-y-2">
           <h3 className="font-bold text-lg md:text-xl lg:text-2xl">{title}</h3>
           <ul className="flex flex-col md:flex-row gap-y-4 md:gap-x-4">
@@ -45,7 +45,7 @@ const Project = ({ title, description, link, github, img, tags }: Project) => {
             ))}
           </ul>
         </div>
-        <p className="lg:w-80">{t(description)}</p>
+        <p className="flex-grow lg:max-w-[480px]">{t(description)}</p>
         <div className="flex flex-col md:flex-row gap-y-4 md:gap-x-4">
           {github && (
             <LinkPill label="Github" href={github}>
